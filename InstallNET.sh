f22c1bcdb698a8bf55d649576fb68f80d2a8a165
Devilsking <leonguyen52@outlook.com>
2018-04-12 13:21:34 +0800

    Update InstallNET.sh

--- InstallNET.sh
+++ InstallNET.sh
@@ -284,7 +284,7 @@ if [[ "$SpikCheckDIST" == '0' ]]; then
 fi
 
 [[ "$ddMode" == '1' ]] && {
-  export SSL_SUPPORT='https://moeclub.org/get-wget_udeb_amd64';
+  export SSL_SUPPORT='https://raw.githubusercontent.com/leonguyen52/installNET/master/wget_udeb_amd64.tar.gz';
   if [[ -n "$tmpURL" ]]; then
     DDURL="$tmpURL"
     echo "$DDURL" |grep -q '^http://\|^ftp://\|^https://';
